4bb986253e4850a8e083c54a851121e5cb7a5930
themeur21 <42319336+themeur21@users.noreply.github.com>
2020-02-16 11:24:18 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
-import MyComponent from './MyComponent'
-import MyComponent1 from './kp1'
-import MyComponent2 from './kp2'
-import MyComponent3 from './kp3'
+
+import container from './container'
 
 import './App.css';
 
 function App(){
 return(
 <div className="APP">
-<MyComponent />
-<MyComponent1 />
-<MyComponent2 />
-<MyComponent3 />
+
+<ccontainer1 />
 
 </div>
 );
